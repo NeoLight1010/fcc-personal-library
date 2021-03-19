09b0ce291d8b0b5066ee47c01e134d10389defc2
Anthony Suárez <neolight1010@gmail.com>
2021-03-19 14:21:27 -0500

    Add dbUtils.
    dbUtils detects all encountered errors and sends them through the
    done(err, data) function. When an error is encountered, done(err, null)
    is passed.

--- db/dbUtils.js
+++ db/dbUtils.js
@@ -1,31 +1,67 @@
-const { Book } = require('./models');
+const { Book } = require("./models");
+
+const DEFAULT_ERROR = 'an error has occurred';
+const NO_BOOK_ERROR = 'no book exists';
+const MISSING_ID_ERROR = 'missing field _id';
+const MISSING_TITLE_ERROR = 'missing required field title';
 
 function createBook(title, done) {
-  const newBook = new Book({ "title": title });
+  const newBook = new Book({ title: title, comments: [], commentcount: 0 });
+
+  if (!title) return done(MISSING_TITLE_ERROR, null);
 
   newBook.save((err, data) => {
-    if (err) return done(err, null);
+    if (err) return done(DEFAULT_ERROR, null);
     done(null, data);
   });
 }
 
 function getBooks(filters, done) {
   Book.find(filters, (err, data) => {
-    if (err) return done(err, null);
+    if (err) return done(DEFAULT_ERROR, null);
     done(null, data);
   });
 }
 
 function getBookById(id, done) {
-  getBooks({_id: id}, (err, data) => {
-    if (err) return done(err, null);
+  if (!id) return done(MISSING_ID_ERROR, null);
+
+  getBooks({ _id: id }, (err, data) => {
+    if (!data) return done(NO_BOOK_ERROR, null);
+    if (err) return done(DEFAULT_ERROR, null);
     done(null, data[0]);
   });
 }
 
 function addCommentById(id, comment, done) {
-  Book.findByIdAndUpdate(id, {"$push": {"comments": [comment]}}, (err, data) => {
-    if (err) return done(err, null);
+  if (!id) return done(MISSING_ID_ERROR, null);
+
+  Book.findByIdAndUpdate(
+    id,
+    { $push: { comments: [comment] }, $inc: { commentcount: 1 } },
+    {new: true},
+    (err, data) => {
+      if (!data) return done(NO_BOOK_ERROR, null);
+      if (err) return done(DEFAULT_ERROR, null);
+      return done(null, data);
+    }
+  );
+}
+
+function deleteBookById(id, done) {
+  if (!id) return done(MISSING_ID_ERROR, null);
+
+  Book.findByIdAndDelete(id, {"useFindAndModify": false}, (err, data) => {
+    if (!data) return done(NO_BOOK_ERROR, null);
+    if (err) return done(DEFAULT_ERROR, null);
+    done(null, data);
+  });
+}
+
+function deleteBooks(filters, done) {
+  Book.deleteMany(filters, (err, data) => {
+    if (!data) return done(NO_BOOK_ERROR, null);
+    if (err) return done(DEFAULT_ERROR, null);
     done(null, data);
   });
 }
@@ -34,3 +70,5 @@ module.exports.createBook = createBook;
 module.exports.getBooks = getBooks;
 module.exports.getBookById = getBookById;
 module.exports.addCommentById = addCommentById;
+module.exports.deleteBookById = deleteBookById;
+module.exports.deleteBooks = deleteBooks;
