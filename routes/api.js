78017a774fc164da929d9e08263ff5165042f098
Anthony Suárez <neolight1010@gmail.com>
2021-03-19 14:23:03 -0500

    Complete API endpoint.

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,7 @@
 */
 
 'use strict';
+const dbUtils = require('../db/dbUtils');
 
 module.exports = function (app) {
 
@@ -14,15 +15,27 @@ module.exports = function (app) {
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      dbUtils.getBooks({}, (err, data) => {
+        if (err) return res.send(err);
+        return res.json(data);
+      })
     })
     
     .post(function (req, res){
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
+      dbUtils.createBook(title, (err, data) => {
+        if (err) return res.send(err);
+        return res.json(data);
+      });
     })
     
     .delete(function(req, res){
       //if successful response will be 'complete delete successful'
+      dbUtils.deleteBooks({}, (err, data) => {
+        if (err) return res.send(err);
+        return res.send("complete delete successful");
+      })
     });
 
 
@@ -31,17 +44,30 @@ module.exports = function (app) {
     .get(function (req, res){
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      dbUtils.getBookById(bookid, (err, data) => {
+        if (err) return res.send(err);
+        return res.json(data);
+      });
     })
     
     .post(function(req, res){
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
+      if (!comment) return res.send("missing required field comment");
+      dbUtils.addCommentById(bookid, comment, (err, data) => {
+        if (err) return res.send(err);
+        return res.json(data);
+      })
     })
     
     .delete(function(req, res){
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
+      dbUtils.deleteBookById(bookid, (err, data) => {
+        if (err) return res.send(err);
+        return res.send("delete successful");
+      });
     });
   
 };
